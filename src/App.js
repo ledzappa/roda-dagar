8be27b54a6cb25c01f6301aad6133219ff163eee
Mikael Juntti <juntti.mikael@gmail.com>
2020-09-01 22:33:11 +0200

    wip

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import './App.css';
 
+// rörliga: påsk, pingst, kristihimmel, midsommar, alla helgons
+//
+
 const svDays = [
+  'söndag',
   'måndag',
   'tisdag',
   'onsdag',
   'torsdag',
   'fredag',
   'lördag',
-  'söndag',
 ];
 
 const svMonthAbbr = [
@@ -40,8 +43,16 @@ const redDays2020 = [
     name: 'Långfredagen',
   },
   {
+    date: '2020-04-11',
+    name: 'Påskafton',
+  },
+  {
     date: '2020-04-12',
     name: 'Påskdagen',
+  },
+  {
+    date: '2020-04-13',
+    name: 'Annandag påsk',
   },
   {
     date: '2020-05-01',
@@ -58,6 +69,11 @@ const redDays2020 = [
   {
     date: '2020-06-06',
     name: 'Nationaldagen',
+  },
+  {
+    date: '2020-06-19',
+    name: 'Midsommarafton',
+    notReallyRed: true,
   },
   {
     date: '2020-06-20',
@@ -86,28 +102,49 @@ const redDays2020 = [
   },
 ];
 
+const getSqueezeDays = (day) => {
+  const weekDayNo = day.date.getDay();
+  if (weekDayNo === 2 || weekDayNo === 4) {
+    const d = new Date(day.date.getTime() + 86400000 * (weekDayNo === 2 ? -1 : 1));
+    if (
+      redDays2020.filter((day) => new Date(day.date).getTime() === d.getTime())
+        .length === 0
+    ) {
+      const squeezeDay = {
+        date: d,
+        isSqueezeDay: true,
+        name: 'Klämdag',
+      };
+      return weekDayNo === 2 ? [squeezeDay, day] : [day, squeezeDay];
+    }
+  }
+
+  return [day];
+};
+
 function App() {
   return (
     <div className="App">
       <h1>Röda dagar</h1>
       {redDays2020
         .map((day) => ({ ...day, date: new Date(day.date) }))
-        .map((day) => ({ ...day, dayName: svDays[day.date.getDay() - 1] }))
-        .filter((day) => day.date > new Date())
+        .reduce((prev, curr) => [...prev, ...getSqueezeDays(curr)], [])
+        .map((day) => ({ ...day, dayName: svDays[day.date.getDay()] }))
+        //.filter((day) => day.date > new Date())
         .map((day) => (
           <div
             className={
               'day-container mb-1 p-1 text-left d-flex ' +
-              (day.notReallyRed ? 'gray' : 'red')
+              (day.notReallyRed ? 'gray' : day.isSqueezeDay ? 'squeeze' : 'red')
             }
           >
-            <div className="text-center date-box mr-2">
+            <div className="text-center date-box mr-3">
               <h1 className="mb-0">{day.date.getUTCDate()}</h1>
               <h4>{svMonthAbbr[day.date.getMonth()].toUpperCase()}</h4>
             </div>
             <div className="date-name">
               <h2 className="mb-0">
-                {day.name} {day.dayName}
+                {day.name} {day.dayName?.substr(0, 3)}
               </h2>
             </div>
           </div>

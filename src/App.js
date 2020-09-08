8c2ad5f3b9053454497ce64108cff27707b46a88
ledzappa <juntti.mikael@gmail.com>
2020-09-08 22:47:35 +0200

    wip

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
 // rörliga: påsk, pingst, kristihimmel, midsommar, alla helgons
@@ -105,7 +105,9 @@ const redDays2020 = [
 const getSqueezeDays = (day) => {
   const weekDayNo = day.date.getDay();
   if (weekDayNo === 2 || weekDayNo === 4) {
-    const d = new Date(day.date.getTime() + 86400000 * (weekDayNo === 2 ? -1 : 1));
+    const d = new Date(
+      day.date.getTime() + 86400000 * (weekDayNo === 2 ? -1 : 1)
+    );
     if (
       redDays2020.filter((day) => new Date(day.date).getTime() === d.getTime())
         .length === 0
@@ -122,35 +124,54 @@ const getSqueezeDays = (day) => {
   return [day];
 };
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Röda dagar</h1>
-      {redDays2020
-        .map((day) => ({ ...day, date: new Date(day.date) }))
-        .reduce((prev, curr) => [...prev, ...getSqueezeDays(curr)], [])
-        .map((day) => ({ ...day, dayName: svDays[day.date.getDay()] }))
-        //.filter((day) => day.date > new Date())
-        .map((day) => (
-          <div
-            className={
-              'day-container mb-1 p-1 text-left d-flex ' +
-              (day.notReallyRed ? 'gray' : day.isSqueezeDay ? 'squeeze' : 'red')
-            }
-          >
-            <div className="text-center date-box mr-3">
-              <h1 className="mb-0">{day.date.getUTCDate()}</h1>
-              <h4>{svMonthAbbr[day.date.getMonth()].toUpperCase()}</h4>
+class App extends Component {
+  componentDidMount() {
+    this.getEaster(2020);
+  }
+
+  getEaster(year) {
+    console.log(year);
+    // gauss easter formula
+    const [a, b, c] = [year % 19, year % 4, year % 7];
+    const d = (19 * a + 24) % 30;
+    const e = (2 * b + 4 * c + 6 * d + 5) % 7;
+    const f = 22 + d + e;
+    f <= 31 ? console.log(f + ' mars') : console.log(f - 31 + ' april');
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Röda dagar</h1>
+        {redDays2020
+          .map((day) => ({ ...day, date: new Date(day.date) }))
+          .reduce((prev, curr) => [...prev, ...getSqueezeDays(curr)], [])
+          .map((day) => ({ ...day, dayName: svDays[day.date.getDay()] }))
+          // .filter((day) => day.date > new Date())
+          .map((day) => (
+            <div
+              className={
+                'day-container mb-2 text-left d-flex position-relative ' +
+                (day.notReallyRed
+                  ? 'gray'
+                  : day.isSqueezeDay
+                  ? 'squeeze'
+                  : 'red')
+              }
+            >
+              <div className="text-center date-box mr-2">
+                <h1 className="mb-0">{day.date.getUTCDate()}</h1>
+                <h4>{svMonthAbbr[day.date.getMonth()].toUpperCase()}</h4>
+              </div>
+              <div className="date-name">
+                <h2 className="mb-0 pl-3">{day.name}</h2>
+              </div>
+              <div className="day-name">{day.dayName?.substr(0, 3)}</div>
             </div>
-            <div className="date-name">
-              <h2 className="mb-0">
-                {day.name} {day.dayName?.substr(0, 3)}
-              </h2>
-            </div>
-          </div>
-        ))}
-    </div>
-  );
+          ))}
+      </div>
+    );
+  }
 }
 
 export default App;

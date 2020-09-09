3a12f6ebaabe516125e34dcd6b37cc261e0d7304
ledzappa <juntti.mikael@gmail.com>
2020-09-09 17:24:46 +0200

    wip

--- src/App.js
+++ src/App.js
@@ -1,115 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
+import { svDays, svMonthAbbr, staticDays } from './svDates';
 
-// rörliga: påsk, pingst, kristihimmel, midsommar, alla helgons
-//
-
-const svDays = [
-  'söndag',
-  'måndag',
-  'tisdag',
-  'onsdag',
-  'torsdag',
-  'fredag',
-  'lördag',
-];
-
-const svMonthAbbr = [
-  'jan',
-  'feb',
-  'mar',
-  'apr',
-  'maj',
-  'jun',
-  'jul',
-  'aug',
-  'sep',
-  'okt',
-  'nov',
-  'dec',
-];
-
-const redDays2020 = [
-  {
-    date: '2020-01-01',
-    name: 'Nyårsdagen',
-  },
-  {
-    date: '2020-01-06',
-    name: 'Trettondedag jul',
-  },
-  {
-    date: '2020-04-10',
-    name: 'Långfredagen',
-  },
-  {
-    date: '2020-04-11',
-    name: 'Påskafton',
-  },
-  {
-    date: '2020-04-12',
-    name: 'Påskdagen',
-  },
-  {
-    date: '2020-04-13',
-    name: 'Annandag påsk',
-  },
-  {
-    date: '2020-05-01',
-    name: 'Första maj',
-  },
-  {
-    date: '2020-05-21',
-    name: 'Kristi himmelfärdsdag',
-  },
-  {
-    date: '2020-05-31',
-    name: 'Pingstdagen',
-  },
-  {
-    date: '2020-06-06',
-    name: 'Nationaldagen',
-  },
-  {
-    date: '2020-06-19',
-    name: 'Midsommarafton',
-    notReallyRed: true,
-  },
-  {
-    date: '2020-06-20',
-    name: 'Midsommardagen',
-  },
-  {
-    date: '2020-10-31',
-    name: 'Alla helgons dag',
-  },
-  {
-    date: '2020-12-24',
-    name: 'Julafton',
-    notReallyRed: true,
-  },
-  {
-    date: '2020-12-25',
-    name: 'Juldagen',
-  },
-  {
-    date: '2020-12-26',
-    name: 'Annandag jul',
-  },
-  {
-    date: '2021-01-01',
-    name: 'Nyårsdagen',
-  },
-];
+const oneDay = 86400000;
 
 const getSqueezeDays = (day) => {
   const weekDayNo = day.date.getDay();
   if (weekDayNo === 2 || weekDayNo === 4) {
     const d = new Date(
-      day.date.getTime() + 86400000 * (weekDayNo === 2 ? -1 : 1)
+      day.date.getTime() + oneDay * (weekDayNo === 2 ? -1 : 1)
     );
     if (
-      redDays2020.filter((day) => new Date(day.date).getTime() === d.getTime())
+      staticDays.filter((day) => new Date(day.date).getTime() === d.getTime())
         .length === 0
     ) {
       const squeezeDay = {
@@ -117,41 +19,78 @@ const getSqueezeDays = (day) => {
         isSqueezeDay: true,
         name: 'Klämdag',
       };
-      return weekDayNo === 2 ? [squeezeDay, day] : [day, squeezeDay];
+      return [day, squeezeDay];
     }
   }
 
   return [day];
 };
 
-class App extends Component {
-  componentDidMount() {
-    this.getEaster(2020);
-  }
+const getEasterBasedDays = (year) => {
+  // gauss easter formula
+  const [a, b, c] = [year % 19, year % 4, year % 7];
+  const d = (19 * a + 24) % 30;
+  const e = (2 * b + 4 * c + 6 * d + 5) % 7;
+  const f = 22 + d + e;
+  const date = new Date(`${year}-${f <= 31 ? '03-' + f : '04-' + (f - 31)}`);
 
-  getEaster(year) {
-    console.log(year);
-    // gauss easter formula
-    const [a, b, c] = [year % 19, year % 4, year % 7];
-    const d = (19 * a + 24) % 30;
-    const e = (2 * b + 4 * c + 6 * d + 5) % 7;
-    const f = 22 + d + e;
-    f <= 31 ? console.log(f + ' mars') : console.log(f - 31 + ' april');
+  return [
+    { name: 'Långfredagen', date: new Date(date.getTime() - oneDay * 2) },
+    { name: 'Påskafton', date: new Date(date.getTime() - oneDay) },
+    { name: 'Påskdagen', date: date },
+    { name: 'Annandag påsk', date: new Date(date.getTime() + oneDay) },
+    {
+      name: 'Kristihimmelfärdsdag',
+      date: new Date(date.getTime() + oneDay * 39),
+    },
+    { name: 'Pingsdagen', date: new Date(date.getTime() + oneDay * 49) },
+  ];
+};
+
+const getMidsummerDays = (year) => {
+  for (let i = 20; i <= 26; i++) {
+    const date = new Date(year + '-06-' + i);
+    if (date.getDay() === 6) {
+      return [
+        {
+          name: 'Midsommarafton',
+          date: new Date(date.getTime() - oneDay),
+          notReallyRed: true,
+        },
+        {
+          name: 'Midsommardagen',
+          date,
+        },
+      ];
+    }
+  }
+};
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentYear: new Date().getFullYear(),
+    };
   }
 
   render() {
     return (
       <div className="App">
         <h1>Röda dagar</h1>
-        {redDays2020
-          .map((day) => ({ ...day, date: new Date(day.date) }))
+        {[
+          ...staticDays,
+          ...getEasterBasedDays(this.state.currentYear),
+          ...getMidsummerDays(this.state.currentYear),
+        ]
+          .sort((a, b) => a.date - b.date)
           .reduce((prev, curr) => [...prev, ...getSqueezeDays(curr)], [])
           .map((day) => ({ ...day, dayName: svDays[day.date.getDay()] }))
           // .filter((day) => day.date > new Date())
           .map((day) => (
             <div
               className={
-                'day-container mb-2 text-left d-flex position-relative ' +
+                'day-container d-flex position-relative mb-2 text-left ' +
                 (day.notReallyRed
                   ? 'gray'
                   : day.isSqueezeDay
@@ -160,8 +99,8 @@ class App extends Component {
               }
             >
               <div className="text-center date-box mr-2">
-                <h1 className="mb-0">{day.date.getUTCDate()}</h1>
-                <h4>{svMonthAbbr[day.date.getMonth()].toUpperCase()}</h4>
+                <h1 className="mb-0">{day.date?.getUTCDate()}</h1>
+                <h4>{svMonthAbbr[day.date.getMonth()]?.toUpperCase()}</h4>
               </div>
               <div className="date-name">
                 <h2 className="mb-0 pl-3">{day.name}</h2>
---
+++ src/svDates.js
@@ -0,0 +1,60 @@
+export const svDays = [
+  'söndag',
+  'måndag',
+  'tisdag',
+  'onsdag',
+  'torsdag',
+  'fredag',
+  'lördag',
+];
+
+export const svMonthAbbr = [
+  'jan',
+  'feb',
+  'mar',
+  'apr',
+  'maj',
+  'jun',
+  'jul',
+  'aug',
+  'sep',
+  'okt',
+  'nov',
+  'dec',
+];
+
+export const staticDays = [
+  {
+    date: new Date(' -01-01'),
+    name: 'Nyårsdagen',
+  },
+  {
+    date: new Date('2020-01-06'),
+    name: 'Trettondedag jul',
+  },
+  {
+    date: new Date('2020-05-01'),
+    name: 'Första maj',
+  },
+  {
+    date: new Date('2020-06-06'),
+    name: 'Nationaldagen',
+  },
+  {
+    date: new Date('2020-10-31'),
+    name: 'Alla helgons dag',
+  },
+  {
+    date: new Date('2020-12-24'),
+    name: 'Julafton',
+    notReallyRed: true,
+  },
+  {
+    date: new Date('2020-12-25'),
+    name: 'Juldagen',
+  },
+  {
+    date: new Date('2020-12-26'),
+    name: 'Annandag jul',
+  },
+];

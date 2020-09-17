a6571506915930fcee8ea0ca3143108fe34a541f
ledzappa <juntti.mikael@gmail.com>
2020-09-17 14:14:47 +0200

    Updates

---
+++ precache-manifest.383780fe616f4ee443d205669b9deb09.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4206d78c93e9929171b9ea60bc02390c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4810b242d0b8f69d851d",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "cd8c80521cbde5dc6fe7",
+    "url": "/static/css/main.b91d2d9f.chunk.css"
+  },
+  {
+    "revision": "4810b242d0b8f69d851d",
+    "url": "/static/js/2.ae382308.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ae382308.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd8c80521cbde5dc6fe7",
+    "url": "/static/js/main.d6244407.chunk.js"
+  },
+  {
+    "revision": "8192df6132cd7fcaeb6c",
+    "url": "/static/js/runtime-main.b7ce6dc8.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/roda-dagar/precache-manifest.be41c05256b7c69256620cffb0c5fdcf.js"
+  "/precache-manifest.383780fe616f4ee443d205669b9deb09.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/roda-dagar/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
